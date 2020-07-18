3835f6e63502554d2e8c7e6f886eee2e59befee0
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-07-18 12:41:06 +0200

    moved styles to own file (message bar)

---
+++ packages/frontend/src/components/MessageBar/MessageBar.styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+	background: #eeeeee;
+`;
+
+export const CenterWrapper = styled.div`
+	max-width: 1240px;
+	padding: 0 40px;
+	margin: 0 auto;
+`;
+
+export const MessageList = styled.ul`
+	margin: 0;
+	list-style: none;
+	display: flex;
+`;
+
+const Message = styled.li`
+	padding: 9px 80px;
+	flex-basis: 33.33%;
+`;
+
+export const MessageLeft = styled(Message)`
+	padding-left: 0;
+`;
+
+export const MessageMiddle = styled(Message)`
+	text-align: center;
+`;
+
+export const MessageRight = styled(Message)`
+	padding-right: 0;
+	text-align: right;
+`;
+
+export const MessageAnchor = styled.a`
+	font-family: 'Cerebri Sans', sans-serif;
+	font-size: 10px;
+	line-height: 14px;
+	color: #666666;
+	text-transform: uppercase;
+	font-weight: bold;
+	text-decoration: none;
+	border-bottom: 1px solid #666666;
+`;
--- packages/frontend/src/components/MessageBar/MessageBar.view.tsx
+++ packages/frontend/src/components/MessageBar/MessageBar.view.tsx
@@ -1,49 +1,13 @@
 import React from 'react';
-
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-	background: #eeeeee;
-`;
-
-const CenterWrapper = styled.div`
-	max-width: 1240px;
-	padding: 0 40px;
-	margin: 0 auto;
-`;
-
-const MessageList = styled.ul`
-	margin: 0;
-	list-style: none;
-	display: flex;
-`;
-
-const Message = styled.li`
-	padding: 9px 80px;
-	flex-basis: 33.33%;
-`;
-
-const MessageLeft = styled(Message)`
-	padding-left: 0;
-`;
-const MessageMiddle = styled(Message)`
-	text-align: center;
-`;
-const MessageRight = styled(Message)`
-	padding-right: 0;
-	text-align: right;
-`;
-
-const MessageAnchor = styled.a`
-	font-family: 'Cerebri Sans', sans-serif;
-	font-size: 10px;
-	line-height: 14px;
-	color: #666666;
-	text-transform: uppercase;
-	font-weight: bold;
-	text-decoration: none;
-	border-bottom: 1px solid #666666;
-`;
+import {
+	Wrapper,
+	CenterWrapper,
+	MessageAnchor,
+	MessageLeft,
+	MessageList,
+	MessageMiddle,
+	MessageRight,
+} from './MessageBar.styles';
 
 const MessageBarView = () => {
 	return (
