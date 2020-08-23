9c0ee5774f5dda8fdac829eaa18e5e6888ab1514
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-08-23 14:55:51 +0200

    disable some things temporarily

--- packages/backend/src/modules/product/resolvers/category.ts
+++ packages/backend/src/modules/product/resolvers/category.ts
@@ -220,7 +220,8 @@ const resolvers: ProductModuleResolversType = {
 				};
 			}
 
-			const { orderBy, filters } = args;
+			// const { orderBy, filters } = args;
+			const { filters } = args;
 
 			let appliedFilters: FilteredProducts[] = [];
 			if (filters) {
@@ -257,16 +258,16 @@ const resolvers: ProductModuleResolversType = {
 				}
 			});
 
-			let filteredAndOrderedProducts = filteredProducts;
-			if (orderBy) {
-				if (orderBy === 'price_ASC') {
-					filteredAndOrderedProducts = filteredProducts.sort((left, right) => left.price - right.price);
-				}
+			const filteredAndOrderedProducts = filteredProducts;
+			// if (orderBy) {
+			// 	if (orderBy === 'price_ASC') {
+			// 		filteredAndOrderedProducts = filteredProducts.sort((left, right) => left.price - right.price);
+			// 	}
 
-				if (orderBy === 'price_DESC') {
-					filteredAndOrderedProducts = filteredProducts.sort((left, right) => right.price - left.price);
-				}
-			}
+			// 	if (orderBy === 'price_DESC') {
+			// 		filteredAndOrderedProducts = filteredProducts.sort((left, right) => right.price - left.price);
+			// 	}
+			// }
 
 			return {
 				...connectionFromArray(
