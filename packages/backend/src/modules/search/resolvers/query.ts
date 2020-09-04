08e38bf2f5cf51b93125d52db936286541bc44f4
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-09-04 12:57:26 +0200

    update dockerfile and docker repo

--- packages/backend/src/modules/search/resolvers/query.ts
+++ packages/backend/src/modules/search/resolvers/query.ts
@@ -12,14 +12,9 @@ interface SearchTarget extends Node {
 const resolvers: SearchModuleResolversType = {
 	Query: {
 		search: async (_parent, { query }, { injector }) => {
-			console.time('products');
 			const products = await injector.get(ProductProvider).getConfigurableProducts();
-			console.timeEnd('products');
-			console.time('brands');
 			const brands = await injector.get(BrandProvider).getBrands();
-			console.timeEnd('brands');
 
-			console.time('createlist');
 			const listToSearch: SearchTarget[] = [
 				...products.map(product => {
 					return { ...product, __typename: 'Product' };
@@ -28,15 +23,12 @@ const resolvers: SearchModuleResolversType = {
 					return { ...brand, __typename: 'Brand' };
 				}),
 			];
-			console.timeEnd('createlist');
 			const options: Fuzzysort.KeyOptions = {
 				limit: 16,
 				threshold: -10000,
 				key: 'name',
 			};
-			console.time('fuzzy');
 			const results = fuzzysort.go(query, listToSearch, options);
-			console.timeEnd('fuzzy');
 			return {
 				results: results.map(result => {
 					return {
