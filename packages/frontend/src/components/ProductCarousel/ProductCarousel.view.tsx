a7267760f7c980a399cbc0eeedd475097df4a0d6
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-09-07 20:49:23 +0200

    adapt carousel width based on device size

--- packages/frontend/src/components/ProductCarousel/ProductCarousel.view.tsx
+++ packages/frontend/src/components/ProductCarousel/ProductCarousel.view.tsx
@@ -2,90 +2,58 @@ import React, { useState } from 'react';
 
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import { createFragmentContainer, graphql } from 'react-relay';
+import styled from 'styled-components';
 
+import { CenterWrapper } from '../../styles/center-wrapper';
 import { ProductCard } from '../ProductCard';
 
 import { ProductCarousel_products } from './__generated__/ProductCarousel_products.graphql';
 
-interface ProductCarouselViewProps {
-	products: ProductCarousel_products;
-	sidePadding: number;
-}
-
 const CAROUSEL_ITEMS_IN_VIEW = 4;
 const UPPER_ITEM_INDEX_OFFSET = CAROUSEL_ITEMS_IN_VIEW - 1;
 
-const ProductCarouselView = ({ products, sidePadding }: ProductCarouselViewProps) => {
+const ProductsContainer = styled.ul`
+	margin: 0;
+	padding: 0;
+	list-style: none;
+	display: flex;
+	width: 400%;
+	transition: transform 300ms;
+	position: relative;
+
+	@media (min-width: 320px) {
+		width: 400%;
+	}
+
+	@media (min-width: 641px) {
+		width: 200%;
+	}
+
+	@media (min-width: 961px) {
+		width: 133.3%;
+	}
+
+	@media (min-width: 1281px) {
+		width: 100%;
+	}
+`;
+
+interface ProductCarouselViewProps {
+	products: ProductCarousel_products;
+}
+
+const ProductCarouselView = ({ products }: ProductCarouselViewProps) => {
 	const [index, setIndex] = useState(0);
 
 	return (
-		<div style={{ position: 'relative' }}>
-			{index > 0 && (
-				<button
-					style={{
-						position: 'absolute',
-						padding: '8px',
-						background: 'white',
-						border: '1px solid lightgrey',
-						outline: 'none',
-						zIndex: 10,
-						top: 'calc(50% - 48px)',
-						left: `${sidePadding + 8}px`,
-						cursor: 'pointer',
-					}}
-					onClick={() => {
-						setIndex(index => index - 1);
-					}}
-				>
-					<div style={{ width: '32px', height: '32px' }}>
-						<FaAngleLeft size="32" />
-					</div>
-				</button>
-			)}
-			{index + UPPER_ITEM_INDEX_OFFSET < products.edges.length - 1 && (
-				<button
-					style={{
-						position: 'absolute',
-						padding: '8px',
-						background: 'white',
-						border: '1px solid lightgrey',
-						outline: 'none',
-						zIndex: 10,
-						top: 'calc(50% - 48px)',
-						right: `${sidePadding + 8}px`,
-						cursor: 'pointer',
-					}}
-					onClick={() => {
-						setIndex(index => index + 1);
-					}}
-				>
-					<div style={{ width: '32px', height: '32px' }}>
-						<FaAngleRight size="32" />
-					</div>
-				</button>
-			)}
+		<div>
 			<div
 				style={{
-					position: 'relative',
 					overflow: 'hidden',
 				}}
 			>
-				<div
-					style={{
-						padding: `0 calc((100vw - 1240px) / 2)`,
-					}}
-				>
-					<ul
-						style={{
-							display: 'flex',
-							listStyle: 'none',
-							margin: '0',
-							width: '200%',
-							padding: '0',
-							transform: `translateX(-${25 * index}%)`,
-							transition: 'all 300ms',
-						}}
-					>
+				<CenterWrapper style={{ position: 'relative' }}>
+					<ProductsContainer style={{ transform: `translateX(-${25 * index}%)` }}>
 						{products.edges.map(({ node: product }, index) => {
 							return (
 								<li
@@ -102,8 +70,52 @@ const ProductCarouselView = ({ products, sidePadding }: ProductCarouselViewProps
 								</li>
 							);
 						})}
-					</ul>
-				</div>
+					</ProductsContainer>
+					{index > 0 && (
+						<button
+							style={{
+								position: 'absolute',
+								padding: '8px',
+								background: 'white',
+								border: '1px solid lightgrey',
+								outline: 'none',
+								zIndex: 10,
+								top: 'calc(50% - 48px)',
+								left: '8px',
+								cursor: 'pointer',
+							}}
+							onClick={() => {
+								setIndex(index => index - 1);
+							}}
+						>
+							<div style={{ width: '32px', height: '32px' }}>
+								<FaAngleLeft size="32" />
+							</div>
+						</button>
+					)}
+					{index + UPPER_ITEM_INDEX_OFFSET < products.edges.length - 1 && (
+						<button
+							style={{
+								position: 'absolute',
+								padding: '8px',
+								background: 'white',
+								border: '1px solid lightgrey',
+								outline: 'none',
+								zIndex: 10,
+								top: 'calc(50% - 48px)',
+								right: '8px',
+								cursor: 'pointer',
+							}}
+							onClick={() => {
+								setIndex(index => index + 1);
+							}}
+						>
+							<div style={{ width: '32px', height: '32px' }}>
+								<FaAngleRight size="32" />
+							</div>
+						</button>
+					)}
+				</CenterWrapper>
 			</div>
 		</div>
 	);
