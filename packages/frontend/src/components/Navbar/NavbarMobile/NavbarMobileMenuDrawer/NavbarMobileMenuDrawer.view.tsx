a8461f33dbc3abe44408f08539b1815bb7872e2e
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-10-08 03:09:21 +0200

    mobile menu drawer design

--- packages/frontend/src/components/Navbar/NavbarMobile/NavbarMobileMenuDrawer/NavbarMobileMenuDrawer.view.tsx
+++ packages/frontend/src/components/Navbar/NavbarMobile/NavbarMobileMenuDrawer/NavbarMobileMenuDrawer.view.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import Drawer from 'rc-drawer';
 import { Collapse } from 'react-collapse';
-import { FaAngleDown, FaAngleUp, FaTimes } from 'react-icons/fa';
+import { FaAngleDown, FaAngleRight, FaAngleUp, FaTimes } from 'react-icons/fa';
 import { createFragmentContainer, graphql } from 'react-relay';
 import styled from 'styled-components';
 
@@ -14,42 +14,34 @@ import { NORMAL_TABLET_SIZE } from '../../../../utils/device-size';
 import { NavbarMobileMenuDrawer_query } from './__generated__/NavbarMobileMenuDrawer_query.graphql';
 
 const Header = styled.div`
-	position: relative;
-	padding: 0.5em 0;
-	text-align: center;
-	font-size: 1.5em;
-	line-height: 1.5em;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 48px;
 `;
 
 const CloseButton = styled.button`
 	position: absolute;
-	right: 0.5em;
-	top: 0.25em;
 	background: white;
 	border: none;
 	outline: none;
-	padding: 0.5em;
-	font-size: 1em;
 	cursor: pointer;
-`;
-
-const Banner = styled.div`
-	height: 128px;
-	background-image: url(/images/tennis-banner.jpg);
-	background-position: right;
+	right: 12px;
+	padding: 12px;
 `;
 
 const OuterCategoryList = styled.ul`
 	margin: 0;
 	padding: 0;
 	list-style: none;
+	background: white;
 `;
 
 const OuterCategoryListItem = styled.li`
-	padding: 1em 0.5em;
+	padding-left: 20px;
 
 	&:not(:last-child) {
-		border-bottom: 1px solid black;
+		border-bottom: 1px solid #ddd;
 	}
 `;
 
@@ -61,8 +53,8 @@ const OuterCategoryListItemHeader = styled.a`
 	color: black;
 	text-decoration: none;
 	flex-grow: 1;
-	padding: 1em;
-	font-weight: bold;
+	padding: 20px 0;
+	font-size: 20px;
 `;
 
 const OuterCategoryListItemCaretWrapper = styled.button`
@@ -76,19 +68,22 @@ const OuterCategoryListItemCaretWrapper = styled.button`
 
 const InnerCategoryList = styled.ul`
 	margin: 0;
-	padding: 0;
+	padding: 0 0 0 30px;
 	list-style: none;
 `;
 
 const InnerCategoryListItem = styled.li`
-	border-top: 1px solid black;
+	&:not(:last-child) {
+		border-bottom: 1px solid #ddd;
+	}
 `;
 
 const CategoryLink = styled.a`
 	color: black;
 	text-decoration: none;
-	padding: 1em 1em 1em 1.5em;
+	padding: 20px 0;
 	display: block;
+	font-size: 20px;
 `;
 
 interface NavbarMobileMenuDrawerViewProps {
@@ -111,56 +106,121 @@ const NavbarMobileMenuDrawerView = ({ query, open, onCloseRequest }: NavbarMobil
 			width={windowWidth > NORMAL_TABLET_SIZE ? NORMAL_TABLET_SIZE + 'px' : '100%'}
 			onClose={onCloseRequest}
 		>
-			<Header>
-				Meny
-				<CloseButton onClick={onCloseRequest}>
-					<IconWrapper size="1em">
-						<FaTimes size="1em" />
-					</IconWrapper>
-				</CloseButton>
-			</Header>
-			<Banner />
-			<OuterCategoryList>
-				{query.rootCategories.map(category => {
-					const isOpened = expandedCategories.includes(category.id);
+			<div style={{ display: 'flex', height: '100%', flexDirection: 'column' }}>
+				<Header>
+					<img src="/images/greenladies-logo.png" alt="" style={{ height: '24px' }} />
+					<CloseButton onClick={onCloseRequest}>
+						<IconWrapper size="24px">
+							<FaTimes size="24px" />
+						</IconWrapper>
+					</CloseButton>
+				</Header>
+				<div style={{ background: '#eee', flexGrow: 1 }}>
+					<div
+						style={{
+							textTransform: 'uppercase',
+							color: '#555',
+							fontWeight: 'bold',
+							fontSize: '14px',
+							margin: '12px 0 4px 4px',
+						}}
+					>
+						Kategorier
+					</div>
+					<OuterCategoryList>
+						{query.rootCategories.map(category => {
+							const isOpened = expandedCategories.includes(category.id);
 
-					return (
-						<OuterCategoryListItem key={category.id}>
+							return (
+								<OuterCategoryListItem key={category.id}>
+									<OuterCategoryListItemHeaderWrapper>
+										<Link href="/categories/[key]" as={`/categories/${category.urlKey}`} passHref>
+											<OuterCategoryListItemHeader onClick={onCloseRequest}>
+												{category.name}
+											</OuterCategoryListItemHeader>
+										</Link>
+										<OuterCategoryListItemCaretWrapper
+											onClick={() => {
+												if (isOpened) {
+													setExpandedCategories(expandedCategories =>
+														expandedCategories.filter(expandedCategory => expandedCategory !== category.id),
+													);
+												} else {
+													setExpandedCategories(expandedCategories => [...expandedCategories, category.id]);
+												}
+											}}
+										>
+											<IconWrapper size="24px">
+												{isOpened ? <FaAngleUp size="24px" /> : <FaAngleDown size="24px" />}
+											</IconWrapper>
+										</OuterCategoryListItemCaretWrapper>
+									</OuterCategoryListItemHeaderWrapper>
+									<Collapse isOpened={isOpened}>
+										<InnerCategoryList>
+											{category.children.map(innerCategory => (
+												<InnerCategoryListItem key={innerCategory.id}>
+													<Link href="/categories/[key]" as={`/categories/${innerCategory.urlKey}`} passHref>
+														<CategoryLink onClick={onCloseRequest}>{innerCategory.name}</CategoryLink>
+													</Link>
+												</InnerCategoryListItem>
+											))}
+										</InnerCategoryList>
+									</Collapse>
+								</OuterCategoryListItem>
+							);
+						})}
+					</OuterCategoryList>
+					<div
+						style={{
+							textTransform: 'uppercase',
+							color: '#555',
+							fontWeight: 'bold',
+							fontSize: '14px',
+							margin: '12px 0 4px 4px',
+						}}
+					>
+						Service
+					</div>
+					<OuterCategoryList>
+						<OuterCategoryListItem>
 							<OuterCategoryListItemHeaderWrapper>
-								<Link href="/categories/[key]" as={`/categories/${category.urlKey}`} passHref>
-									<OuterCategoryListItemHeader onClick={onCloseRequest}>{category.name}</OuterCategoryListItemHeader>
+								<Link href="/[...slug]?tab=om-green-ladies" as="/kundservice?tab=om-green-ladies" passHref>
+									<OuterCategoryListItemHeader>Om Green Ladies</OuterCategoryListItemHeader>
 								</Link>
-								<OuterCategoryListItemCaretWrapper
-									onClick={() => {
-										if (isOpened) {
-											setExpandedCategories(expandedCategories =>
-												expandedCategories.filter(expandedCategory => expandedCategory !== category.id),
-											);
-										} else {
-											setExpandedCategories(expandedCategories => [...expandedCategories, category.id]);
-										}
-									}}
-								>
+								<OuterCategoryListItemCaretWrapper>
 									<IconWrapper size="24px">
-										{isOpened ? <FaAngleUp size="24px" /> : <FaAngleDown size="24px" />}
+										<FaAngleRight size="24px" />
 									</IconWrapper>
 								</OuterCategoryListItemCaretWrapper>
 							</OuterCategoryListItemHeaderWrapper>
-							<Collapse isOpened={isOpened}>
-								<InnerCategoryList>
-									{category.children.map(innerCategory => (
-										<InnerCategoryListItem key={innerCategory.id}>
-											<Link href="/categories/[key]" as={`/categories/${innerCategory.urlKey}`} passHref>
-												<CategoryLink onClick={onCloseRequest}>{innerCategory.name}</CategoryLink>
-											</Link>
-										</InnerCategoryListItem>
-									))}
-								</InnerCategoryList>
-							</Collapse>
 						</OuterCategoryListItem>
-					);
-				})}
-			</OuterCategoryList>
+						<OuterCategoryListItem>
+							<OuterCategoryListItemHeaderWrapper>
+								<Link href="/[[...slug]]?tab=frakt" as="/kundservice?tab=frakt" passHref>
+									<OuterCategoryListItemHeader>Klimatkompenserad frakt</OuterCategoryListItemHeader>
+								</Link>
+								<OuterCategoryListItemCaretWrapper>
+									<IconWrapper size="24px">
+										<FaAngleRight size="24px" />
+									</IconWrapper>
+								</OuterCategoryListItemCaretWrapper>
+							</OuterCategoryListItemHeaderWrapper>
+						</OuterCategoryListItem>
+						<OuterCategoryListItem>
+							<OuterCategoryListItemHeaderWrapper>
+								<Link href="/[[...slug]]?tab=betalning" as="/kundservice?tab=betalning" passHref>
+									<OuterCategoryListItemHeader>Trygg & säker betalning</OuterCategoryListItemHeader>
+								</Link>
+								<OuterCategoryListItemCaretWrapper>
+									<IconWrapper size="24px">
+										<FaAngleRight size="24px" />
+									</IconWrapper>
+								</OuterCategoryListItemCaretWrapper>
+							</OuterCategoryListItemHeaderWrapper>
+						</OuterCategoryListItem>
+					</OuterCategoryList>
+				</div>
+			</div>
 		</Drawer>
 	);
 };
