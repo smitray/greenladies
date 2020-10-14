5dbab7660ede4abd982344268097a082fdf735e4
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-10-14 11:44:38 +0200

    remove graphql endpoint from next config

--- packages/admin/next.config.js
+++ packages/admin/next.config.js
@@ -1,6 +1,3 @@
 module.exports = {
 	basePath: '/admin',
-	env: {
-		GRAPHQL_API_ENDPOINT: process.env.GRAPHQL_API_ENDPOINT,
-	},
 };
--- packages/frontend/next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	env: {
-		GRAPHQL_API_ENDPOINT: process.env.GRAPHQL_API_ENDPOINT,
-	},
-};
