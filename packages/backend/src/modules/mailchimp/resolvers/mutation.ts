07ffe4b39b1a4a81229bc62cf2cc6a49de15244f
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-10-16 12:50:33 +0200

    fix mailchimp

--- packages/backend/src/modules/mailchimp/resolvers/mutation.ts
+++ packages/backend/src/modules/mailchimp/resolvers/mutation.ts
@@ -1,17 +1,27 @@
 import axios from 'axios';
 
 import { MailchimpModuleResolversType } from '..';
+import { base64 } from '../../../utils/base64';
 
 const resolvers: MailchimpModuleResolversType = {
 	Mutation: {
 		addEmailToSubscriberList: async (_parent, { input }) => {
 			const MAILCHIMP_LIST = String(process.env.MAILCHIMP_LIST);
 			const MAILCHIMP_SERVER = String(process.env.MAILCHIMP_SERVER);
+			const MAILCHIMP_API_TOKEN = String(process.env.MAILCHIMP_API_TOKEN);
 
-			await axios.post(`https://${MAILCHIMP_SERVER}.api.mailchimp.com/3.0/lists/${MAILCHIMP_LIST}/members`, {
-				email_address: input.email,
-				status: 'subscribed',
-			});
+			await axios.post(
+				`https://${MAILCHIMP_SERVER}.api.mailchimp.com/3.0/lists/${MAILCHIMP_LIST}/members`,
+				{
+					email_address: input.email,
+					status: 'subscribed',
+				},
+				{
+					headers: {
+						Authorization: `Basic ${base64('placeholder:' + MAILCHIMP_API_TOKEN)}`,
+					},
+				},
+			);
 
 			return {
 				success: true,
