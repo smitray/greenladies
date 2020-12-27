d4d960ff4d041dac7152bebd73a8a010310e3467
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-12-27 18:39:56 +0100

    Fix facebook pixel code

--- packages/frontend/src/lib/facebook-pixel.ts
+++ packages/frontend/src/lib/facebook-pixel.ts
@@ -1,5 +1,4 @@
-// export const FB_PIXEL_ID = process.env.NODE_ENV === 'production' ? '2717399138495684' : '';
-export const FB_PIXEL_ID = '451305489206609';
+export const FB_PIXEL_ID = process.env.NODE_ENV === 'production' ? '2717399138495684' : '';
 
 export const pageView = () => {
 	if (typeof window !== 'undefined') {
