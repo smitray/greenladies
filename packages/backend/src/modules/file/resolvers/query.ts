0fb2913eea496219422960ced1468907fced4e18
Rasmus Rosengren <rasmus.rosengren@pm.me>
2021-02-14 14:29:24 +0100

    fetch images and data

--- packages/backend/src/modules/file/resolvers/query.ts
+++ packages/backend/src/modules/file/resolvers/query.ts
@@ -1,9 +1,33 @@
+import fs from 'fs';
+import _sizeOf from 'image-size';
+import path from 'path';
+import { promisify } from 'util';
+
 import { FileModuleResolversType } from '..';
+
+const sizeOf = promisify(_sizeOf);
 
 const resolvers: FileModuleResolversType = {
 	Query: {
-		images: () => {
-			return [];
+		images: async () => {
+			const files = fs.readdirSync(path.join(__dirname, '../../../../static/images'));
+			console.log(files);
+			const images = [];
+			for (const file of files) {
+				try {
+					const dimensions = await sizeOf(path.join(__dirname, '../../../../static/images', file));
+					if (dimensions && dimensions.width && dimensions.height) {
+						images.push({
+							path: '/static/images/' + file,
+							width: dimensions.width,
+							height: dimensions.height,
+						});
+					}
+				} catch (err) {
+					// not an image, ignore it
+				}
+			}
+			return images;
 		},
 	},
 };
